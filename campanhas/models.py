316ce65958b57a4ee0497944b7504513ad661306
Leandro Rafael Freire <rafaelleandrofreire@gmail.com>
2020-01-29 14:35:04 -0300

    Template

--- campanhas/models.py
+++ campanhas/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Campanha(models.Model):
     titulo_campanha = models.CharField(max_length=200)
-    imagem = models.ImageField(upload_to='imagens/')
+    imagem = models.ImageField(upload_to='campanhas/imagens/')
