6bdc69837e86a3faf98cbade3eb8ba0883c6f303
Leandro Rafael Freire <rafaelleandrofreire@gmail.com>
2020-01-29 16:49:20 -0300

    Criando um template e renderizando na view

--- adscar/urls.py
+++ adscar/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('campanhas.urls')),
+    path('campanhas/', include('campanhas.urls')),
     path('admin/', admin.site.urls),
     #polls aplication
     path('polls/', include('polls.urls')),
