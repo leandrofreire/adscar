3297ce8fce7d1a21aeef3b3782307f6869adab5d
Leandro Rafael Freire <rafaelleandrofreire@gmail.com>
2020-01-31 11:29:02 -0300

    Imagens não estão funcionando

--- campanhas/models.py
+++ campanhas/models.py
@@ -6,4 +6,3 @@ class Campanha(models.Model):
     objects = None
     titulo_campanha = models.CharField(max_length=200)
     imagem = models.ImageField(upload_to='campanhas/imagens/')
-    #pub_date = models.DateField('date published', default="2020-01-01")
