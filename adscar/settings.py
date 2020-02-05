47d42a50582f1a69f6fe3a16ab8da4c8afd70cdb
Leandro Rafael Freire <rafaelleandrofreire@gmail.com>
2020-02-05 10:40:50 -0300

    Whitenoise to work with statis files

--- adscar/settings.py
+++ adscar/settings.py
@@ -53,7 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'adscar.urls'
 
